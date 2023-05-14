bfbe1483b4c9be9bfa7060548139afcc8b6e1b5f
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2023-05-14 14:19:06 +0800

    frontend: styling the footer

--- App/FrontEnd/src/common/footer/Footer.jsx
+++ App/FrontEnd/src/common/footer/Footer.jsx
@@ -2,5 +2,10 @@ import packageJson from "../../../package.json";
 import styles from "./footer.module.css";
 
 export default function Footer() {
-  return <footer className={styles.footer}>This is footer {packageJson.version} </footer>;
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.content}>&#169; All right reserved</div>
+      <section className={styles.version}>v{packageJson.version} </section>
+    </footer>
+  );
 }
