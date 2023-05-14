1fad1c4cb9d6e977c38f614fbb097e1ee8961e67
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2023-05-14 14:44:14 +0800

    Frontend: added 400 Page

--- App/FrontEnd/src/common/header/Header.jsx
+++ App/FrontEnd/src/common/header/Header.jsx
@@ -12,6 +12,9 @@ export default function Header() {
           <li>
             <Link href="/about">About</Link>
           </li>
+          <li>
+            <Link href="/not-found">Not Found</Link>
+          </li>
         </ul>
       </nav>
     </header>
