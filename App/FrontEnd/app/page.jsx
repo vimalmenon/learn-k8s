3f93f3659119649f74054877d0be6cd280bf2878
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2023-05-14 14:31:22 +0800

    Frontend: Add about page

--- App/FrontEnd/app/page.jsx
+++ App/FrontEnd/app/page.jsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <section>This is Vimal Menon</section>;
+  return <section>This is Home Page</section>;
 }
--- App/FrontEnd/src/common/header/Header.jsx
+++ App/FrontEnd/src/common/header/Header.jsx
@@ -1,5 +1,19 @@
 import styles from "./header.module.css";
+import Link from "next/link";
 
 export default function Header() {
-  return <header className={styles.header}>This is header</header>;
+  return (
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
