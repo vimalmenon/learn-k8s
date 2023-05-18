3706131321776a60615d1e8d1ef84f18d3c6eeef
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2023-05-18 12:12:12 +0800

    More Refactoring of Page

--- App/FrontEnd/src/Page/App.jsx
+++ App/FrontEnd/src/Page/App.jsx
@@ -4,27 +4,27 @@ export default function App(props) {
       <tbody>
         <tr>
           <td>App Name</td>
-          <td>{props.appName}</td>
+          <td>{props.appName || "None"}</td>
         </tr>
         <tr>
           <td>Node Name</td>
-          <td>{props.nodeName}</td>
+          <td>{props.nodeName || "None"}</td>
         </tr>
         <tr>
           <td>Pod Name</td>
-          <td>{props.podName}</td>
+          <td>{props.podName || "None"}</td>
         </tr>
         <tr>
           <td>POD NAMESPACE</td>
-          <td>{props.podNamespace}</td>
+          <td>{props.podNamespace || "None"}</td>
         </tr>
         <tr>
           <td>POD IP</td>
-          <td>{props.podIp}</td>
+          <td>{props.podIp || "None"}</td>
         </tr>
         <tr>
           <td>Env</td>
-          <td>{props.env}</td>
+          <td>{props.env || "None"}</td>
         </tr>
       </tbody>
     </table>
